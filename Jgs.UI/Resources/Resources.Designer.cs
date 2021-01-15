55fd52a4bfe6bb330cac9add570b631de01c4f30
JGS <jan.duchacek.bc@gmail.com>
2021-01-15 10:17:55 +0100

    Opravy po přejmenování. Opravy cest k resource dictionaries komponent

--- Jgs.UI/Resources/Resources.Designer.cs
+++ Jgs.UI/Resources/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Jgs.UI.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Jgs.Framework.UI.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Jgs.UI.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
